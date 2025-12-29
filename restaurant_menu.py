4f309a3fbb088ff554f2973acfc565c83e603111
avugaddijoshna <avugaddijoshna@gmail.com>
2025-12-29 12:11:07 +0530

    Add files via upload

---
+++ restaurant_menu.py
@@ -0,0 +1,12 @@
+print("WELCOME TO PYTHON RESTAURANT")
+print("----------------------------")
+
+print("Menu Card")
+print("1. Idli        - ₹30")
+print("2. Dosa        - ₹40")
+print("3. Vada        - ₹25")
+print("4. Poori       - ₹35")
+print("5. Tea         - ₹10")
+
+print("----------------------------")
+print("Thank you! Visit again 😊")
