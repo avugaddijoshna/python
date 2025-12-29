03f138d68b99962723b30c639ae4e84df37bff55
avugaddijoshna <avugaddijoshna@gmail.com>
2025-12-29 12:25:55 +0530

    Update restaurant_menu.py

--- restaurant_menu.py
+++ restaurant_menu.py
@@ -1,12 +1,18 @@
+menu = {
+    "Idli": 30,
+    "Dosa": 40,
+    "Vada": 25,
+    "Poori": 35,
+    "Tea": 10
+}
+
 print("WELCOME TO PYTHON RESTAURANT")
 print("----------------------------")
-
 print("Menu Card")
-print("1. Idli        - ₹30")
-print("2. Dosa        - ₹40")
-print("3. Vada        - ₹25")
-print("4. Poori       - ₹35")
-print("5. Tea         - ₹10")
+
+for item, price in menu.items():
+    print(f"{item:<10} - ₹{price}")
 
 print("----------------------------")
 print("Thank you! Visit again 😊")
+
